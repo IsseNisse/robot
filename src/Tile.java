79fd6bc7567f3ee47fd918bafe18a1c6f9d51efd
Schampen <hampus.mann@elev.ga.ntig.se>
2019-12-17 09:27:38 +0100

    Tile setters and getters

--- src/Tile.java
+++ src/Tile.java
@@ -1,2 +1,35 @@
 public class Tile {
+    private boolean robot;
+    private boolean light;
+    private boolean food;
+
+    public Tile(boolean robot, boolean food, boolean light) {
+        this.robot = robot;
+        this.food = food;
+        this.light = light;
+    }
+
+    public boolean isRobot() {
+        return robot;
+    }
+
+    public void setRobot(boolean robot) {
+        this.robot = robot;
+    }
+
+    public boolean isLight() {
+        return light;
+    }
+
+    public void setLight(boolean light) {
+        this.light = light;
+    }
+
+    public boolean isFood() {
+        return food;
+    }
+
+    public void setFood(boolean food) {
+        this.food = food;
+    }
 }
