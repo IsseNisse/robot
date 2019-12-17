bb89e871d06bfac6ff6a3091bfb6a734e1d4fa81
Schampen <hampus.mann@elev.ga.ntig.se>
2019-12-17 10:30:54 +0100

    kommentering

--- src/Tile.java
+++ src/Tile.java
@@ -3,33 +3,75 @@ public class Tile {
     private boolean light;
     private boolean food;
 
+    /**
+     *
+     * @param robot - håller koll på om det finns en robot eller ej på tilen
+     * @param food - håller koll på om det finns mat eller ej på tilen
+     * @param light - håller koll på om det finns ljus eller ej på tilen
+     */
     public Tile(boolean robot, boolean food, boolean light) {
         this.robot = robot;
         this.food = food;
         this.light = light;
     }
 
+    /**
+     *
+     * @return - return värdet på robot
+     */
     public boolean isRobot() {
         return robot;
     }
 
+    /**
+     *
+     * @param robot - ändrar värdet på robot
+     */
     public void setRobot(boolean robot) {
         this.robot = robot;
     }
 
+    /**
+     *
+     * @return - return värdet på light
+     */
     public boolean isLight() {
         return light;
     }
 
+    /**
+     *
+     * @param light - ändrar värdet på light
+     */
     public void setLight(boolean light) {
         this.light = light;
     }
 
+    /**
+     *
+     * @return - return värdet på food
+     */
     public boolean isFood() {
         return food;
     }
 
+    /**
+     *
+     * @param food - ändrar värdet på food
+     */
     public void setFood(boolean food) {
         this.food = food;
+    }
+
+    /**
+     *
+     * @return - returnar true om det finns både en robot och mat på en tile
+     */
+    public boolean isEat() {
+        boolean eat = false;
+        if (this.food && this.robot) {
+            eat = true;
+        }
+        return eat;
     }
 }
