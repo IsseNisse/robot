98d307d5df7fbca369ba275d5f7da50a5cd60bc7
Schampen <hampus.mann@elev.ga.ntig.se>
2020-01-21 08:56:48 +0100

    wHY SIGHT NO WORK

---
+++ src/Ball.java
@@ -0,0 +1,17 @@
+public class Ball {
+    int x;
+    int y;
+    int vX;
+    int vY;
+    int size;
+    String color;
+
+    public Ball(int x, int y, int vX, int vY, int size, String colot) {
+        this.x = x;
+        this.y  y;
+        this.vX = vX;
+        this.vY = vY;
+        this.size = size;
+        this.color = color;
+    }
+}
