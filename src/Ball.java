4e163884a049f29a4d5b549ff333f24a220abcb3
Schampen <hampus.mann@elev.ga.ntig.se>
2020-01-21 09:14:17 +0100

    BALL TIME

--- src/Ball.java
+++ src/Ball.java
@@ -6,12 +6,19 @@ public class Ball {
     int size;
     String color;
 
-    public Ball(int x, int y, int vX, int vY, int size, String colot) {
+    public Ball(int x, int y, int vX, int vY, int size, String color) {
         this.x = x;
-        this.y  y;
+        this.y = y;
         this.vX = vX;
         this.vY = vY;
         this.size = size;
         this.color = color;
+    }
+
+    public void move() {
+        this.x = this.x + this.vX;
+        this.y = this.y + this.vY;
+        this.vX = this.vX - 1;
+        this.vY = this.vY - 1;
     }
 }
